a0b84f7fc5cc0a071360e9e567294fc74cae277f
dafda1 <dafda1@st-andrews.ac.uk>
2026-07-08 11:43:45 +0100

    add invert_matrix function
    analytical matrix inverter, works as np.linalg.inv wrapper if dim > 3

--- AuxFuncs.py
+++ AuxFuncs.py
@@ -45,3 +45,34 @@ def translate_reflections (hkl_array, space = "reciprocal"):
     text = text[:-2] + end_text
     
     return text
+
+def invert_matrix (square_matrix):
+    shape = square_matrix.shape
+    if (len(shape) != 2) or (shape[0] != shape[1]):
+        raise ValueError(f"Array not a square matrix, shape is f{shape}")
+        
+    n = shape[0]
+        
+    if n > 3: 
+        return np.linalg.inv(square_matrix)
+    
+    elif n == 1:
+        return 1.0/square_matrix
+    
+    else:
+        Vol = np.linalg.det(square_matrix)
+        
+        if n == 2:
+            adjugate = np.array((square_matrix[1, 1], -square_matrix[1, 0]),
+                                (-square_matrix[0, 1], square_matrix[0, 0]))
+        
+        elif n == 3:
+            adjugate = np.empty(shape, dtype = float)
+            for i in np.arange(3):
+                adjugate[:, i] = np.cross(square_matrix[(i + 1) % 3],
+                                          square_matrix[(i + 2) % 3])
+        
+        else:
+            return np.linalg.inv(square_matrix)
+            
+        return adjugate/Vol
